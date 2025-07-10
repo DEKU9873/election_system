727dc155b7ee7f8faf19dabd872bd8c26f16eb21
DEKU9873 <alhassan9873@gmail.com>
2025-07-10 14:36:17 +0300

    v29

--- election_system/src/Components/auth/UserTableHeader.jsx
+++ election_system/src/Components/auth/UserTableHeader.jsx
@@ -10,7 +10,8 @@ const UserTableHeader = ({
   handleSort 
 }) => {
   return (
-    <thead className="bg-gray-50">
+    
+    <thead className="bg-sky-700">
       <tr>
         {tableHeaders.map((header) => {
           if (!visibleColumns[header.id]) return null;
@@ -29,11 +30,11 @@ const UserTableHeader = ({
           }
 
           return (
-            <th key={header.id} className="px-2 sm:px-4 py-2 sm:py-3 text-right">
+            <th key={header.id} className="px-2 sm:px-4 py-2 sm:py-3 text-right ">
               {header.sortable ? (
                 <button
                   onClick={() => handleSort(header.id)}
-                  className="flex items-center gap-1 text-xs sm:text-sm font-medium text-gray-900 hover:text-gray-700"
+                  className="flex items-center gap-1 text-xs sm:text-sm font-medium text-white hover:text-gray-700"
                 >
                   {header.icon && <header.icon className="w-3 sm:w-4 h-3 sm:h-4" />}
                   <span className="hidden sm:inline">{header.label}</span>
@@ -41,7 +42,7 @@ const UserTableHeader = ({
                   <ArrowUpDown className="w-3 sm:w-4 h-3 sm:h-4" />
                 </button>
               ) : (
-                <span className="text-xs sm:text-sm font-medium text-gray-900">
+                <span className="text-xs sm:text-sm font-medium text-white">
                   <span className="hidden sm:inline">{header.label}</span>
                   <span className="inline sm:hidden">{header.shortLabel || header.label}</span>
                 </span>
