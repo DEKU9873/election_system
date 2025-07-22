6e9a4f04308d8832d333efb8cdf461704a63aed1
DEKU9873 <alhassan9873@gmail.com>
2025-07-22 17:50:54 +0300

    v48

--- election_system/vite.config.js
+++ election_system/vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true // 👈 هذا يجعل السيرفر مفتوح للشبكة
-  }
+
 })
